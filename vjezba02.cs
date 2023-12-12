5fc892f1a92fafbeacc4e611b0957c060fd7b2ed
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2023-12-12 12:19:53 +0100

    Add files via upload

---
+++ vjezba02.cs
@@ -0,0 +1,14 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int a = 10;
+		int b = 7;
+		double c = 12.5;
+		double d = 16.7;
+		double x = (a + b + c + d)/4;
+		Console.WriteLine("Prosječna vrijednost: {0}", x);
+	}
+}
